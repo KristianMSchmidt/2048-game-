53b090f9e4372757572b03cd8bf1f9551b0a8d39
Kristian Møller Schmidt <34898026+KristianMSchmidt@users.noreply.github.com>
2020-12-02 12:56:58 +0100

    Improved heuristic function

--- backend/heuristic.py
+++ backend/heuristic.py
@@ -2,7 +2,7 @@
 This is the heuristic used by the mini-max algorithm, when evaluating
 grid-positions. 
 
-Given a grid, the quality of the grid is estimated as
+Given a grid, the quality of the grid is estimated as a combination of 
 the monotonicity-score of the grid TIMES the square sum of the grid.
 This ensures both monotonicity and an effort to merge high value tiles whenever possible.
 
@@ -14,11 +14,16 @@ def square_sum(grid):
     """
     Calcultates the sum of the squared tile-values.
     """
-    s = 0
+    squaresum = 0
+    max_tile = 2
     for row in range(4):
         for col in range(4):
-                s += grid.get_tile(row,col)**2
-    return s
+            tile_val = grid.get_tile(row, col) 
+            squaresum += tile_val**2
+            if tile_val > max_tile:
+                max_tile = tile_val
+            
+    return max_tile, squaresum
 
 def heuristic(grid):
     """
@@ -26,6 +31,10 @@ def heuristic(grid):
     """
     if len(grid.get_available_moves()) == 0:
         return -float('inf')  # game over is bad
-    
-    return gradient_heuristic(grid) * square_sum(grid)
+    gh = gradient_heuristic(grid)
+    max_tile, ss = square_sum(grid)
+
+    score = gh*max_tile/10 + ss   # I multiply the gradient score (monotonicity) by the value of 
+                                  # max_tile/10 to ensure a somewhat constant ratio between gh and ss
+    return score
   
