6b2b555a5890530135b552c6731918833d54ea7b
KristianMSchmidt <krms@kvuc.dk>
2021-07-04 10:06:29 +0200

    Ajax calls and visible directions. Fixes #4

--- app.py
+++ app.py
@@ -11,61 +11,44 @@ from backend.Game import Game
 
 app = Flask('__name__')
 
-TIME_LIMIT = 0.6 # How much time does AI-player have pr move?
+TIME_LIMIT = 1.0 # How much time does AI-player have pr move?
+DIR_DICT = {"u": UP, "d": DOWN, "l": LEFT, "r": RIGHT}      
+DIR_TO_ARROW = {1: "&uarr;", 2: "&darr;", 3: "&larr;", 4: "&rarr;"}
 
-
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
 def index():
-    if request.method == 'GET':
-        action = "new_game"
-   
-    else: 
-        data = json.loads(request.form["json_data"])   #return(json.dumps(data))
-        action = data["requested_action"]
+    return render_template("index.html")
 
-    if action == 'new_game': 
-        grid = Grid(4,4)
-        #grid._map = [[2,3,8,16],[32,64,128,256],[512,1024,2048,4096],[8192,2,3,0]]
-        
-        data = {
-            "agent": "human",
-            "grid": grid._map,
-            "game_over": False,
-            "requested_action": "new_game"
-        }
-        
-    elif action == 'run_ai':
-        data['agent'] = 'ai'
-        grid = Grid(4,4)
-        grid._map = data["grid"]
-        game = Game(grid, time_limit = TIME_LIMIT) 
-        direction, info = game.make_ai_move()
-        data["grid"] = game.grid._map
-        print(game.grid._map)
-        data["search_info"] = info    
-        if game.game_over: 
-            data["game_over"] = True
-            data["agent"] = "human"
+@app.route("/new_game", methods=['GET'])
+def new_game():
+    data = {"grid": Grid(4,4)._map}     
+    # data = {"grid": [[2, 4, 8, 16], [32, 64, 128, 512], [1024, 2048, 4096, 0], [0, 0, 0, 0]]}
+    return data
 
-        return render_template("grid.html", data = data)
+@app.route("/ai_move", methods=['POST'])
+def ai_move():
     
-    elif action == 'stop_ai':
-        data['agent'] = 'human'
-    
-    elif action == 'human_move':
+    grid = Grid(4,4)
+    grid._map = json.loads(request.form['grid'])
+    game = Game(grid, time_limit = TIME_LIMIT) 
+    direction, info = game.make_ai_move()
+    data = {"grid": game.grid._map, "direction":DIR_TO_ARROW[direction]}
+    if game.game_over:         
+        data["game_over"] = True
+    return data
 
-        dir_dict ={"u": UP, "d": DOWN, "l": LEFT, "r": RIGHT}      
-        direction = dir_dict[data["direction"]]
-        
-        grid = Grid(4,4)
-        grid._map = data["grid"]
-        game = Game(grid)
-        game.make_human_move(direction)
-        if game.game_over: 
-            data["game_over"] = True
-        data["grid"] = game.grid._map     
-
-    return render_template("index.html", data = data)
+@app.route("/human_move", methods=['POST'])
+def human_move():
+    #direction = json.loads(request.form['direction'])
+    direction = DIR_DICT[request.form['direction']]
+    grid = Grid(4,4)
+    grid._map = json.loads(request.form['grid'])
+    game = Game(grid, time_limit = TIME_LIMIT) 
+    game.make_human_move(direction)
+    data = {"grid": game.grid._map, "direction":DIR_TO_ARROW[direction]}
+    if game.game_over: 
+        data["game_over"] = True
+    return data 
 
 if __name__ == "__main__":
     app.run(debug=True)
